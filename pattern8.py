cd25237bd7f9743bd3fee03cf38d3f778ab62487
ashachandel <40863073+ashachandel@users.noreply.github.com>
2018-08-10 20:45:00 +0530

    pattern_8

---
+++ pattern8.py
@@ -0,0 +1,9 @@
+for i in range(1,6):
+    
+    
+   for j  in range(1,10):
+        if(j<=i or j>=(10-i)):
+          print(i,end=" ")
+        else:
+          print(" ",end=" ")
+   print("\n")  
