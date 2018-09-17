16de6e51394542dde265fb7669e9ab9cb273df64
ashachandel <40863073+ashachandel@users.noreply.github.com>
2018-09-17 21:55:01 +0530

    convert_lowercase_to_uppercase

---
+++ conver_lowercase_to_uppercase.py
@@ -0,0 +1,7 @@
+print("Enter 'x' for exit.");
+string = input("Enter any string to convert in uppercase: ");
+if string == 'x':
+    exit();
+else:
+    string_in_uppercase = string.upper();
+    print("\nString in Uppercase =",string_in_uppercase);
