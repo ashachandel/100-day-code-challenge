8acfa699a151c37b4c82459973beb33a8a4afc63
ashachandel <40863073+ashachandel@users.noreply.github.com>
2018-09-26 15:11:54 +0530

    deleted

--- octal_to_hexadecimal.py
+++
@@ -1,8 +0,0 @@
-print("Enter 'x' for exit.");
-octal = input("Enter a number in Octal Format: ");
-if octal == 'x':
-    exit();
-else:
-    dec = str(int(octal, 8));
-    decm = int(dec);
-    print(octal,"in Hexadecimal =",hex(decm));
